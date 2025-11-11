aebb209d19a03be95b1c8661e9aadd1661bed482
unknown <unai.arevalo.7e8@itb.cat>
2025-11-11 10:18:52 +0100

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bloc1Activitat9
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("""
+                Hello!!!!
+                 /\_/\  n
+                { º^º }||
+                 |\_/|//
+                  U U 
+                """);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# BLOC1.AC09 - Arrays, Matrius i Jagged Arrays
+
+Aquest repositori és una activitat per practicar arrays, matrius i jagged arrays. Cada branca és un exercici, i el README d'aquella branca conte l'enunciat de l'exercici corresponent.
