470f90fdcf7d65368e6466cccd8c57620dcd00d6
unknown <unai.arevalo.7e8@itb.cat>
2025-11-11 12:02:51 +0100

    Exercice three done

--- Program.cs
+++ Program.cs
@@ -6,13 +6,28 @@ namespace Bloc1Activitat9
     {
         public static void Main()
         {
-            Console.WriteLine("""
-                Hello!!!!
-                 /\_/\  n
-                { º^º }||
-                 |\_/|//
-                  U U 
-                """);
+            const string DisplayNumbers = "\nEls nombres generats són: ";
+            const string DisplayReversOrder = "\nEls nombres amb l'ordre invertit:";
+            const string Number = " {0} |";
+            const int Min = 1;
+            const int Max = 10; //the max value is 9, but to use it with the random you need the next number, in this case, 10
+            const int NumsAmount = 20;
+            Random random = new Random();
+            int[] nums = new int[NumsAmount];
+
+            Console.WriteLine(DisplayNumbers);
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                nums[i] = random.Next(Min, Max);
+                Console.Write(Number, nums[i]);
+            }
+
+            Console.WriteLine(DisplayReversOrder);
+            for (int i = nums.GetLength(0) - 1; i >= 0; i--)
+            {
+                Console.Write(Number, nums[i]);
+            }
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# BLOC1.AC09 - Arrays, Matrius i Jagged Arrays
-
-Aquest repositori és una activitat per practicar arrays, matrius i jagged arrays. Cada branca és un exercici, i el README d'aquella branca conte l'enunciat de l'exercici corresponent.
-
-- Els exercicis 3 i 4 estaran fets en la mateixa branca, en la que el 3 serà l'original i el 4 un commit actualitzant-lo
+### Exercici 3
+Especifica un algorisme que generi 10 nombres enters (compresos entre 1 i 9 inclosos). Cal que l’algorisme imprimeixi els valors de dreta a esquerra i després d’esquerra a dreta.
